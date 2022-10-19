26351c1f73c98d3b62534d132e6fd0701243a21d
theoldtree <yjeongmu@gmail.com>
2022-10-19 11:51:02 +0900

    motor fan modifying

--- motor_fan.c
+++ motor_fan.c
@@ -1,7 +1,8 @@
 #include <avr/io.h>
-#define F_CPU
+#define F_CPU 16000000UL
 #define __DELAY_BACKWARD_COMPATIBLE__
 #include <util/delay.h>
+#include <avr/interrupt.h>
 #define STOP_SPEED 0
 #define LOW_SPEED 77
 #define MID_SPEED 153
@@ -10,7 +11,6 @@
 #define MOTOR_CCW 0x70
 #define MOTOR_BREAK 0xd0
 #define MOTOR_STOP 0x30
-STBY = 1
 #define MOTOR_STANDBY 0x00
 #define ON 1
 #define BREAK 0
@@ -18,54 +18,62 @@ STBY = 1
 #define WEAK 0
 #define MID 1
 #define STR 2
-STBY = 0 
-volatile int count = 0, value = 0;
-volatile int state = OFF;
+volatile int count = 0;
 volatile int strength = WEAK;
-int motor_speed[3] = {LOW_SPEED,MID_SPEED,HIGH_SPEED}
-
+volatile int state = BREAK;
+int motor_speed[3] = {LOW_SPEED,MID_SPEED,HIGH_SPEED};
 ISR(INT4_vect){
-    -_delay_ms(100);
-    EIFR |= 0x10;
-    if((PINE & 0x10) == 0x10) return;
-    if(state == ON) state = BREAK;
-    else if(state == BREAK) == ON;
+   _delay_ms(100);
+   EIFR |= 0x10;
+   if((PINE & 0x10) == 0x10) return;
+   if(state == ON) state = BREAK;
+   else if (state == BREAK | state==STOP) state = ON;
 }
 ISR(INT5_vect){
-    -_delay_ms(100);
-    EIFR |= 0x20;
-    if((PINE & 0x20) == 0x20) return;
-    if(motor_speed == WEAK) motor_speed = MID;
-    else if(motor_speed == MID) motor_speed = STR;
-    else motor_speed = WEAK;
+   _delay_ms(100);
+   EIFR |= 0x20;
+   if((PINE & 0x20) == 0x20) return;
+   if(strength == WEAK) strength = MID;
+   else if(strength == MID) strength = STR;
+   else if (strength == STR) strength = WEAK;
 }
 ISR(INT6_vect){
-    -_delay_ms(100);
-    EIFR |= 0x40;
-    if((PINE & 0x40) == 0x40) return;
-    state = STOP;
+   _delay_ms(100);
+   EIFR |= 0x40;
+   if((PINE & 0x40) == 0x40) return;
+   state=STOP;
+}
+ISR(TIMER0_OVF_vect){
+   // TCNT0 = 49;
 }
 
 int main(void){
-    DDRA = 0x0f;
-    DDRB = 0xf0;
-    PORTB = MOTOR_STANDBY;
-    sei();
-    EIMSK = 0x70;
-    EICRB = 0x2a;
-    TCCR2 = 0x6b;
-    PORTB = MOTOR_CW;
-    while(1){
-        if(state == ON){
-            PORTB = MOTOR_CW;
-            OCR2 = motor_speed[strength];
-        }
-        else if(state == BREAK){
-            PORTB = MOTOR_BREAK;
-        }
-        else{
-            _delay_ms(5000);
-            PORTB = MOTOR_STOP;
-        } 
-    }
+   DDRA = 0x0f;
+   DDRB = 0xf0;
+   PORTB = MOTOR_STANDBY;
+   sei();
+   EIMSK = 0x70;
+   EICRB = 0x2a;
+   TCCR2 = 0x6b;
+   PORTB = MOTOR_CW;
+   TCCR0 = 0x07; // 1024 분주
+   /* 타이머 클럭의 주기는 64us
+   원하는 시간 5 000 000 us
+   따라서 필요한 클록 사이클
+   5000000/64 = 78125
+   */
+   TIMSK = 0x01;
+   while(1){
+      if(state == ON){
+         PORTB = MOTOR_CW;
+         OCR2 = motor_speed[strength];
+      }
+      else if(state == BREAK){
+         PORTB = MOTOR_BREAK;
+      }
+      else if(state == STOP){
+        // TCNT0 = 0;
+         PORTB = MOTOR_BREAK;
+      }
+   }
 }
